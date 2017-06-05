679feba0690a1d16ec81ed9c10cd39556c72e251
Bryan Useche <bryan.useche.10@gmail.com>
2017-06-05 09:19:00 -0400

    Subtitle Finder.py
    Update the code, miss the last part: getting url to download subtitle

--- Subtitle finder.py
+++ Subtitle finder.py
@@ -6,30 +6,24 @@ url = 'https://www.tusubtitulo.com/series.php'
 url2 = 'https://www.tusubtitulo.com'
 h = 'https://www.tusubtitulo.com/ajax_loadShow.php?'
 
-serie = list()
+listado_serie = {}
 html = urllib.urlopen(url)
+print "Getting Data"
 soup = BeautifulSoup(html,'html.parser')
-for i in soup('a'):
+for i in soup.find_all('a'):
 	link = i.get('href')
 	if link.startswith('/show/'):
-		serie.append(link)
 		nombre_serie = i.get_text()
-		print nombre_serie
+		listado_serie[nombre_serie] = link 
 		
-for j in serie:
-	tvshow = re.findall('[0-9]+', j)
-	for index in tvshow:
-		serie.append(index)
+		
+print listado_serie
+#Consulta
 
-cont = 0
-num_season = 1
-while x < len(serie):
-	tv_show = serie[cont]
-	cont = cont + 1
-	campos = urllib.urlencode({'show': tv_show , 'season': season})
-	url3 = urllib.urlopen(h , campos)
-	num_season = num_season + 1
-	soup = BeautifulSoup(url3 , 'html.parser')
-	print soup
-	for i in soup('a'):
-		print i
+serie = raw_input("Introduzca el nombre de la serie: ")
+if listado_serie.has_key(serie) == True:
+	print "Encontrado " + serie
+	tv_show = url2 + listado_serie[serie]
+	print tv_show
+else:
+	print "No Encontrado, Reintente"
